43ccfb2971a69aa13c5e235f9f8df6be3a0e82cf
Ben Gordon <benharrisgordon@gmail.com>
2024-01-26 11:04:58 +0100

    Completed challenge

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
   return (
-    <main>
-      <h1>Hello Next!</h1>
-    </main>
+    <>
+      <main>
+        <h1>Hello Next!</h1>
+      </main>
+    </>
   );
 }
 
